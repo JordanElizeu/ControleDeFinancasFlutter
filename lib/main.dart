d80f63f3348afd5dbf89d068535a8fe0931a6a32
Jordan <jordan.elizeu77@gmail.com>
2021-12-08 00:50:07 -0300

    Projeto básico para desenvolvimento

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:app_financeiro/ui/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Home(),
+    );
+  }
+}
